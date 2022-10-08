06eeb471847641044964f2f79976e7502152c15f
henz15 <henrykamwamba@email.com>
2022-10-08 14:00:08 +0200

    Added ocelot.json configurations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
+var webHost = new WebHostBuilder()
+    .UseKestrel()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config
+            .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+            .AddJsonFile("ocelot.json")
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices(s => {
+        s.AddOcelot();
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        //add your logging
+    })
+    .UseIISIntegration()
+    .Configure(app =>
+    {
+        app.UseOcelot().Wait();
+    });
 
+var app = webHost.Build();
 app.Run();
